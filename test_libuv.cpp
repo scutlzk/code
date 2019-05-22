bc84ac962aaefb9dcf60363e630ef7f44afba482
scutlzk <l1005152@gmail.com>
2019-05-22 16:55:49 +0800

    Create test_libuv.cpp

---
+++ test_libuv.cpp
@@ -0,0 +1,93 @@
+﻿//https://github.com/scutlzk/asd.git
+#include <stdio.h>
+#include <stdlib.h>
+#include <uv.h>
+#include <time.h>
+#define DEFAULT_PORT 7000
+#define DEFAULT_BACKLOG 128
+uv_loop_t *loop;
+struct sockaddr_in addr;
+int64_t count = 0;
+double start, end, cost;
+void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+{
+	buf->base = (char*)malloc(suggested_size);
+	buf->len = suggested_size;
+}
+void echo_write(uv_write_t *req, int status)
+{
+	if (status)
+	{
+		fprintf(stderr, "Write error %s.\n", uv_strerror(status));
+	}
+	free(req);
+}
+void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
+{
+	if (nread < 0)
+	{
+		if (nread != UV_EOF)
+			fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+		uv_close((uv_handle_t*)client, NULL);
+	}
+	else if (nread > 0)
+	{
+		uv_write_t *req = (uv_write_t*)malloc(sizeof(uv_write_t));
+		uv_buf_t wrbuf = uv_buf_init(buf->base, nread);
+		++count;
+		cost = clock() - start;
+
+		if (cost > 10000)
+		{
+			printf("%I64u\n", count);
+			printf("%f\n", cost);
+			start = clock();
+			count = 0;
+		}
+		uv_write(req, client, &wrbuf, 1, echo_write);
+	}
+	if (buf->base)
+		free(buf->base);
+}
+void on_new_connection(uv_stream_t *server, int status)
+{
+	if (status < 0)
+	{
+		fprintf(stderr, "On new connection error %s.\n", uv_strerror(status));
+		return;
+	}
+	printf("new connection is coming...\n");
+	uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+	uv_tcp_init(loop, client);
+	if (uv_accept(server, (uv_stream_t*)client) == 0)
+	{
+		uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+	}
+	else
+	{
+		uv_close((uv_handle_t*)client, NULL);
+	}
+}
+
+int main()
+{
+	loop = uv_default_loop();
+
+	uv_tcp_t server;
+
+	uv_tcp_init(loop, &server);
+
+	uv_ip4_addr("172.28.64.10", DEFAULT_PORT, &addr);
+
+	uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
+
+	start = clock();
+
+	int r = uv_listen((uv_stream_t*)&server, DEFAULT_BACKLOG, on_new_connection);
+	if (r)
+	{
+		fprintf(stderr, "Listen error %s.\n", uv_strerror(r));
+		return 1;
+	}
+	return uv_run(loop, UV_RUN_DEFAULT);
+}
